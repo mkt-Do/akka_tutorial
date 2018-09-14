273525a2b9d1753af785431119a4d51a0f42237b
mkt-Do <mkt.koro.mikuru@gmail.com>
2018-09-14 10:17:05 +0900

    life cycle

--- src/main/scala/jp/mkt/akka/Main.scala
+++ src/main/scala/jp/mkt/akka/Main.scala
@@ -3,6 +3,7 @@ package jp.mkt.akka
 import akka.actor.{ Actor, ActorSystem, Props }
 import scala.io.StdIn
 
+/*
 import jp.mkt.akka.actors.MyActor
 
 object Main extends App {
@@ -11,6 +12,19 @@ object Main extends App {
   val firstRef = system.actorOf(Props[MyActor], "first-actor")
   println(s"First Actor: $firstRef")
   firstRef ! "printit"
+  println(">>> Press ENTER to exit <<<")
+  try StdIn.readLine()
+  finally system.terminate()
+}
+*/
+
+import jp.mkt.akka.actors.{ StartStopActor1, StartStopActor2 }
+
+object Main extends App {
+  val system = ActorSystem("mySystem")
+
+  val first = system.actorOf(Props[StartStopActor1], "first")
+  first ! "stop"
   println(">>> Press ENTER to exit <<<")
   try StdIn.readLine()
   finally system.terminate()
---
+++ src/main/scala/jp/mkt/akka/actors/StartStopActor.scala
@@ -0,0 +1,21 @@
+package jp.mkt.akka.actors
+
+import akka.actor.{ Actor, Props }
+
+class StartStopActor1 extends Actor {
+  override def preStart(): Unit = {
+    println("first started")
+    context.actorOf(Props[StartStopActor2], "second")
+  }
+  override def postStop(): Unit = println("first stopped")
+  override def receive: Receive = {
+    case "stop" => context.stop(self)
+  }
+}
+
+class StartStopActor2 extends Actor {
+  override def preStart(): Unit = println("second started")
+  override def postStop(): Unit = println("second stopped")
+  override def receive: Receive = Actor.emptyBehavior
+}
+
